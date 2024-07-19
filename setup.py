f2d5ff978fff575bdea1119ce2fc435598e481fb
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-07-19 10:21:58 -0300

    Ajustes

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    keywords=['Python', 'Streamlit', 'Authentication', 'Components'],
+    keywords=["Python", "Streamlit", "Authentication", "Components"],
     python_requires=">=3.6",
     install_requires=[
         "PyJWT >=2.3.0",
         "bcrypt >= 3.1.7",
         "PyYAML >= 5.3.1",
         "streamlit >= 1.25.0",
-        "extra-streamlit-components >= 0.1.70"
+        "extra-streamlit-components >= 0.1.70",
     ],
 )
